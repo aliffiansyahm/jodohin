4daa0ff4848f782dbacbf0e7899dcc38f5d165f2
aja-x <ajax.soft.en@gmail.com>
2019-05-20 12:59:41 +0700

    Update frontend

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -2,40 +2,42 @@
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
-/* @var $model \common\models\LoginForm */
-
-use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
-
-$this->title = 'Login';
-$this->params['breadcrumbs'][] = $this->title;
+/* @var $model SignupForm */
+use frontend\models\SignupForm;
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+<?=$this->render('../layouts/login-logout/header')?>
+<section class="pb_cover_v3 overflow-hidden cover-bg-indigo cover-bg-opacity text-left pb_gradient_v1 pb_slant-light" id="section-home">
+    <div class="container">
+        <div class="row align-items-center justify-content-center">
+            <div class="col-md-5 relative align-self-center">
 
-    <p>Please fill out the following fields to login:</p>
+                <form action="#" class="bg-white rounded pb_form_v1">
+                    <h2 class="mb-4 mt-0 text-center">Login</h2>
+                    <div class="form-group">
+                        <input type="text" class="form-control pb_height-50 reverse" name="" placeholder="email">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control pb_height-50 reverse" placeholder="password">
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary btn-lg btn-block pb_btn-pill  btn-shadow-blue" value="register">
+                    </div>
+                </form>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-                <?= $form->field($model, 'password')->passwordInput() ?>
-
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
-                <div style="color:#999;margin:1em 0">
-                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
-                    <br>
-                    Need new verification email? <?= Html::a('Resend', ['site/resend-verification-email']) ?>
+            </div>
+            <div class="col-md-1">
+            </div>
+            <div class="col-md-6">
+                <h2 class="heading mb-3">Jodoh.in</h2>
+                <div class="sub-heading">
+                    <p class="mb-4">Temukan jodohmu disini!</p>
+                    <!--                    <p class="mb-5"><a class="btn btn-success btn-lg pb_btn-pill smoothscroll" href="#section-pricing"><span class="pb_font-14 text-uppercase pb_letter-spacing-1">See Pricing</span></a></p>-->
                 </div>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-
-            <?php ActiveForm::end(); ?>
+            </div>
         </div>
     </div>
-</div>
+</section>
+<!-- END section -->
+
+<!-- loader -->
+<div id="pb_loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#1d82ff"/></svg></div>
