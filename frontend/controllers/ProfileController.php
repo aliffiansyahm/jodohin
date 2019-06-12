ce61707e6877a832c2c6e4602293c7c776dd7057
aja-x <ajax.soft.en@gmail.com>
2019-06-12 12:19:40 +0700

    Profile update

--- frontend/controllers/ProfileController.php
+++ frontend/controllers/ProfileController.php
@@ -3,26 +3,39 @@
 namespace frontend\controllers;
 
 use frontend\models\Pengguna;
+use yii\db\Query;
+use yii\web\Controller;
 
-class ProfileController extends \yii\web\Controller
+class ProfileController extends Controller
 {
-    public function actionIndex()
+    public function actionIndex($id = null)
     {
-        // $model = Pengguna::find()
-        // ->where(['EMAIL' => $email,
-        //         'PASSWORD' => $password])
-        // ->one();
-
-        // return $this->render('index',[
-        //     'pengguna' => $model,
-        // ]);
-
-        return $this->render('index');
+        if (is_null($id)) {
+            $id = $_SESSION['id'];
+        }
+        $pengguna = (new Query())
+            ->from('pengguna')
+            ->where('IDPENGGUNA=:id_pengguna', [':id_pengguna' => $id])
+            ->one();
+        return $this->render('index', compact('pengguna'));
     }
 
     public function actionUpdate($id)
     {
 
     }
+
+    public function actionFollowers($id = null)
+    {
+        if (is_null($id)) {
+            $id = $_SESSION['id'];
+        }
+        $pengguna = (new Query())
+            ->from('follow')
+            ->where('IDPENGGUNA=:id_pengguna', [':id_pengguna' => $id])
+            ->all();
+        return $this->render('index', compact('pengguna'));
+    }
+
 
 }
---
+++ frontend/views/test/test/test.php
@@ -0,0 +1,6 @@
+<?php
+foreach ($test as $item) {
+    var_dump($item);
+    echo "<br>";
+    echo $item->NAMA;
+}
